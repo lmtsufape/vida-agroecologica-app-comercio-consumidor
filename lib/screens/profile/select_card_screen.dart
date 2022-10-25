86d99d217af5fcaa1735f3f98fbecc9e6fc65691
JoseDanielF <danielduartefilho.df@gmail.com>
2022-10-24 23:37:24 -0300

    Tela de seleção do cartão de crédito adicionada

---
+++ lib/screens/profile/select_card_screen.dart
@@ -0,0 +1,195 @@
+import 'package:ecommercebonito/components/utils/horizontal_spacer_box.dart';
+import 'package:ecommercebonito/screens/screens_index.dart';
+import 'package:flutter/material.dart';
+
+import '../../components/utils/vertical_spacer_box.dart';
+import '../../shared/components/dialogs/finish_dialog.dart';
+import '../../shared/constants/app_enums.dart';
+
+class SelectCard extends StatelessWidget {
+  const SelectCard({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Ecommerce Bonito',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orange,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, Screens.profile);
+              },
+            ), //IconButton
+          ],
+          //IconButton
+        ),
+        body: Container(
+            width: size.width,
+            padding: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+                child: Column(children: [
+              const VerticalSpacerBox(size: SpacerSize.small),
+              Row(
+                children: const [
+                  Text(
+                    'Informações de pagamento',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const VerticalSpacerBox(size: SpacerSize.large),
+              InkWell(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 440,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 5), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Wrap(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                width: 35.0,
+                                height: 55.0,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                              const Text(
+                                'Forma de pagamento',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 3),
+                            child: Row(
+                              children: const [
+                                HorizontalSpacerBox(size: SpacerSize.large),
+                                Text('Mastercard',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    )),
+                                Text(' (Crédito) ',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold)),
+                                Text('com final 1447',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    )),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 3),
+                            child: Row(
+                              children: const [
+                                HorizontalSpacerBox(size: SpacerSize.large),
+                                Text('Parcelas não disponíveis',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                onTap: () async {},
+              ),
+              const VerticalSpacerBox(size: SpacerSize.medium),
+              InkWell(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 440,
+                      height: 75,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 5), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Wrap(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 35.0,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                                const Text(
+                                  'Adicione um novo cartão',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.start,
+                                ),
+                                const Spacer(),
+                                IconButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(
+                                          context, Screens.payment);
+                                    },
+                                    icon: const Icon(
+                                      Icons.arrow_forward_ios_outlined,
+                                      color: Colors.grey,
+                                    )),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, Screens.payment);
+                },
+              ),
+              
+            ]))));
+  }
+}
